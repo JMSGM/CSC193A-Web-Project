ebb370828d207bed6fec0c360104f260de7847d0
JMSagum <112904712+JMSGM@users.noreply.github.com>
2025-09-19 15:03:58 -0700

    implement fanciy and boring radios

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,6 +1,37 @@
 const button = document.getElementById("big-button");
-const txtArea = document.getElementById("txt-area")
+const mooButton = document.getElementById("moo-button");
+const txtArea = document.getElementById("txt-area");
+const fancyRadio = document.getElementById("fancy-radio")
+const boringRadio = document.getElementById("boring-radio")
+
+fancyRadio.addEventListener("change", styleRadios)
+boringRadio.addEventListener("change", styleRadios)
+
+function styleRadios(){
+    if(fancyRadio.checked){
+        txtArea.style.fontFamily = '"Times New Roman", Times, serif';
+        txtArea.style.fontWeight = "Bold";
+        txtArea.style.fontStyle = "Italic";
+    }
+    if(boringRadio.checked){
+        txtArea.style.fontFamily = "Arial, Helvetica, sans-serif";
+        txtArea.style.fontWeight = "";
+        txtArea.style.fontStyle = "";
+    }
+}
+
+
 button.addEventListener('click', alertButton);
 function alertButton(){
     txtArea.style.fontSize = "24px";
+}
+
+mooButton.addEventListener('click', mooify);
+function mooify(){
+    const chr = txtArea.value;
+    const length = txtArea.length;
+
+    for(let i = 0; i < length; i++){
+        console.log(text[i]);
+    }
 }
